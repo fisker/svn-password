a966db21a71b32e8491602819447099d42cd9439
fisker Cheung <lionkay@gmail.com>
2021-03-30 16:27:06 +0800

    Init

---
+++ cli.mjs
@@ -0,0 +1,49 @@
+#!/usr/bin/node
+
+import path from 'path';
+import fs from 'fs/promises';
+import { unprotectData } from 'win-dpapi';
+import prettyjson from 'prettyjson';
+import boxen from 'boxen';
+
+const realmDirectory = path.join(
+  process.env.APPDATA,
+  'Subversion',
+  'auth',
+  'svn.simple'
+);
+const files = await fs.readdir(realmDirectory);
+for (const hash of files) {
+  const result = await readFile(path.join(realmDirectory, hash));
+  if (result.passtype === 'wincrypt') {
+    try {
+      result.passwordDecrypted = unprotectData(
+        Buffer.from(result.password, 'base64'),
+        null,
+        'CurrentUser'
+      ).toString();
+    } catch {}
+  }
+
+  console.log(
+    boxen(hash + '\n\n' + prettyjson.render(result), { padding: 1, margin: 1 })
+  );
+}
+
+async function readFile(file) {
+  const content = await fs.readFile(file, 'utf8');
+  return Object.fromEntries(parseContent(content));
+}
+
+function* parseContent(content) {
+  const lines = content.split('\n');
+  for (let lineNumber = 0; lineNumber < lines.length; lineNumber += 4) {
+    const [first, second, third, fourth] = lines.slice(
+      lineNumber,
+      lineNumber + 4
+    );
+    if (/^K \d+$/.test(first) && /^V \d+$/.test(third)) {
+      yield [second, fourth];
+    }
+  }
+}
