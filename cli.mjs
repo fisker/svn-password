417920593eb856b2fc437a0c8db4472d939c96f7
fisker Cheung <lionkay@gmail.com>
2021-03-30 19:22:09 +0800

    Node.js v12

--- cli.mjs
+++ cli.mjs
@@ -1,49 +1,55 @@
 #!/usr/bin/node
 
-import path from 'path';
-import fs from 'fs/promises';
-import { unprotectData } from 'win-dpapi';
-import prettyjson from 'prettyjson';
-import boxen from 'boxen';
+import path from 'path'
+import {promises as fs} from 'fs'
+import { unprotectData } from 'win-dpapi'
+import prettyjson from 'prettyjson'
+import boxen from 'boxen'
 
 const realmDirectory = path.join(
   process.env.APPDATA,
   'Subversion',
   'auth',
   'svn.simple'
-);
-const files = await fs.readdir(realmDirectory);
-for (const hash of files) {
-  const result = await readFile(path.join(realmDirectory, hash));
-  if (result.passtype === 'wincrypt') {
-    try {
-      result.passwordDecrypted = unprotectData(
-        Buffer.from(result.password, 'base64'),
-        null,
-        'CurrentUser'
-      ).toString();
-    } catch {}
-  }
+)
 
-  console.log(
-    boxen(hash + '\n\n' + prettyjson.render(result), { padding: 1, margin: 1 })
-  );
-}
+;(async function run() {
+  const files = await fs.readdir(realmDirectory)
+  for (const hash of files) {
+    const result = await readFile(path.join(realmDirectory, hash))
+    if (result.passtype === 'wincrypt') {
+      try {
+        result.passwordDecrypted = unprotectData(
+          Buffer.from(result.password, 'base64'),
+          null,
+          'CurrentUser'
+        ).toString()
+      } catch {}
+    }
+
+    console.log(
+      boxen(hash + '\n\n' + prettyjson.render(result), {
+        padding: 1,
+        margin: 1,
+      })
+    )
+  }
+})()
 
 async function readFile(file) {
-  const content = await fs.readFile(file, 'utf8');
-  return Object.fromEntries(parseContent(content));
+  const content = await fs.readFile(file, 'utf8')
+  return Object.fromEntries(parseContent(content))
 }
 
 function* parseContent(content) {
-  const lines = content.split('\n');
+  const lines = content.split('\n')
   for (let lineNumber = 0; lineNumber < lines.length; lineNumber += 4) {
     const [first, second, third, fourth] = lines.slice(
       lineNumber,
       lineNumber + 4
-    );
+    )
     if (/^K \d+$/.test(first) && /^V \d+$/.test(third)) {
-      yield [second, fourth];
+      yield [second, fourth]
     }
   }
 }
